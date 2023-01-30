a46299d3e4e45404c553afd969ef301061305331
skm5786 <sumitthakral3@gmail.com>
2023-01-30 14:32:12 +0530

    Initial Commit

---
+++ Operating system lab college/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Operating system lab college
+//
+//  Created by sumit thakral on 30/01/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
